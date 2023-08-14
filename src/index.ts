ad6b436057aed847985f6a701d31776fd258b5bc
Alejandra Duque <m.aleja3000@gmail.com>
2023-08-14 18:58:30 -0400

    Add Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express, {Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = '3000';
 
 app.get('/', (req: Request, res: Response) => {
-    console.log('👉👉👉👉 Endpoint working');
-    res.send('👉👉👉👉 Endpoint working');
-
+  console.log('👉👉👉👉 Endpoint working');
+  res.send('👉👉👉👉 Endpoint working');
 });
 
 app.listen(port, () => {
-    console.log('👨‍💻 Server is working!');
+  console.log('👨‍💻 Server is working!');
 });
