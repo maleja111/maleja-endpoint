21af0d8dda2e63302ade2ee9ea853e89842ee423
Alejandra Duque <m.aleja3000@gmail.com>
2023-08-15 18:32:12 -0400

    Add Postgres Connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { Client } from 'ts-postgres';
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const pgUrl = new URL(DATABASE_URL as string);
+const client = new Client({
+  user: pgUrl.username,
+  host: pgUrl.host,
+  database: pgUrl.pathname.replace('/', ''),
+  password: pgUrl.password,
+  port: 5432,
+  keepAlive: true
+});
 
 const app = express();
-const port = '3000';
+const port = process.env.PORT || '3000';
 
-app.get('/', (req: Request, res: Response) => {
-  console.log('👉👉👉👉 Endpoint working');
+
+app.get('/', async (req: Request, res: Response) => {
+  await client.connect();
+  const result = await client.query('SELECT $1::text as message', ['Hello world!']);
+  console.log('conecto');
+  console.log(result.rows[0]);
+  await client.end();
   res.status(200).send('👉👉👉👉 Endpoint working');
 });
 
 app.listen(port, () => {
-  console.log('👨‍💻 Server is working!');
+  console.log(`👨‍💻 Server is working! Port: ${port}`);
 });
